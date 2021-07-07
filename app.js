5d2a2b7b283415631068725ae8e5c0884056a2bd
DbaracaldoP <molonodad@gmail.com>
2021-07-07 14:37:31 -0500

    cambios subi sobre otra rama y realice la union y esta me debolvio lo scambios de ayer

--- app.js
+++ app.js
@@ -16,15 +16,9 @@ app.listen(port, function() {
 app.get('/paginax', function(request, response) {
     response.send('hola mundo desde la app con otro dato hora 11:23');
 });
-console.log('dentro de rutas');
-var bodyParser = require('body-parser');
-const { response } = require('express');
-app.use(bodyParser.json()); //configuracion para decir que soporta fomratos Json en app
-app.use(bodyParser.urlencoded({ extended: true })); //que viene extendido de la codificacion de la url
-//el primer paso quedo en app.js
 //segundo paso manejo del post y el get, el post maneja los datos ocultos no vistos por el cliente y el get maneja los datos por al url http://google/13/jhon/6555  las dos primeras son del dominio y las otras son parametros
 //el primer parametro de la funcion es un oido y el otro es una respuesta
-app.get('/paginax/suma/:valor1/:valor2/paginax/suma/:valor1/:valor2', function(request, response) {
+app.get('/paginax/suma/:valor1/:valor2/', function(request, response) {
     console.log('dentro de rutas suma ');
     var suma = parseInt(request.params.valor1) + parseInt(request.params.valor2)
     response.send('hola mundo desde la app con otro dato hora 1:46 pm suma: ' + suma);
